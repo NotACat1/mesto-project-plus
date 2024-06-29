dcb209025ad4e787026429b015149538114814ed
NotACat1 <notacat.mail@gmail.com>
2024-06-29 13:18:16 +0300

    feat: настройка проекта

---
+++ @types/express/index.d.ts
@@ -0,0 +1,14 @@
+import { ObjectId } from 'mongoose';
+import jwt from 'jsonwebtoken';
+
+declare global {
+  namespace Express {
+    interface Request {
+      // user: { _id: string }
+      user: jwt.JwtPayload
+    }
+    interface Error {
+      statusCode?: number
+    }
+  }
+}
