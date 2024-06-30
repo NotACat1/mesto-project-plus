17ce30dd962676697e53f09cbe2e8f51ee8a5b4f
NotACat1 <notacat.mail@gmail.com>
2024-06-30 17:16:29 +0300

    fix: исправлены `middlewares`

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,15 +1,13 @@
-import { Request, Response, NextFunction } from 'express';
+import { Request, Response } from 'express';
 
 import CustomError from '@utils/CustomError';
 
 // Middleware для обработки ошибок
-const errorHandler = (err: CustomError, req: Request, res: Response, next: NextFunction) => {
+const errorHandler = (err: CustomError, req: Request, res: Response) => {
   const { statusCode = 500, message } = err;
 
   res.status(statusCode).send({
-    message: statusCode === 500
-      ? 'На сервере произошла ошибка'
-      : message
+    message: statusCode === 500 ? 'На сервере произошла ошибка' : message,
   });
 };
 
--- src/middlewares/logger.ts
+++ src/middlewares/logger.ts
@@ -9,41 +9,35 @@ const logger = winston.createLogger({
     new winston.transports.Console({
       format: winston.format.combine(
         winston.format.colorize(), // Раскрашивание уровней логов
-        winston.format.simple() // Простой формат вывода
-      )
+        winston.format.simple(), // Простой формат вывода
+      ),
     }),
     // Транспорт для записи в файл request.log
     new winston.transports.File({ filename: 'request.log', level: 'info' }),
     // Транспорт для записи в файл error.log
-    new winston.transports.File({ filename: 'error.log', level: 'error' })
-  ]
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+  ],
 });
 
 // Middleware для логирования запросов
 export const requestLogger = expressWinston.logger({
   transports: [
     new winston.transports.Console(),
-    new winston.transports.File({ filename: 'request.log' })
+    new winston.transports.File({ filename: 'request.log' }),
   ],
-  format: winston.format.combine(
-    winston.format.colorize(),
-    winston.format.json()
-  ),
+  format: winston.format.combine(winston.format.colorize(), winston.format.json()),
   meta: true, // Включаем метаданные запроса (например, IP и URL)
   msg: 'HTTP {{req.method}} {{req.url}}', // Шаблон сообщения лога
-  expressFormat: true // Используем стандартный формат express
+  expressFormat: true, // Используем стандартный формат express
 });
 
 // Middleware для логирования ошибок
 export const errorLogger = expressWinston.errorLogger({
   transports: [
     new winston.transports.Console(),
-    new winston.transports.File({ filename: 'error.log' })
+    new winston.transports.File({ filename: 'error.log' }),
   ],
-  format: winston.format.combine(
-    winston.format.colorize(),
-    winston.format.json()
-  )
+  format: winston.format.combine(winston.format.colorize(), winston.format.json()),
 });
 
 export default logger;
